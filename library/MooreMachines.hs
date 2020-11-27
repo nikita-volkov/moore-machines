d64060cfaf964a300e57d3ddb51efeb14b457e0a
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-11-27 12:49:12 +0300

    Implement feedTextChars

--- library/MooreMachines.hs
+++ library/MooreMachines.hs
@@ -9,6 +9,7 @@ import qualified Data.Text.Internal.Private as TextPrivate
 import qualified Data.Text.Internal.Unsafe.Char as TextChar
 import qualified Data.Text.Array as TextArray
 import qualified Data.Text as Text
+import qualified MooreMachines.Util.TextArray as TextArrayUtil
 
 
 feedFoldable :: Foldable f => f a -> Moore a b -> Moore a b
@@ -19,3 +20,15 @@ feedFoldable foldable moore =
       next (progress a)
     finalize =
       id
+
+feedTextChars :: Text -> Moore Char o -> Moore Char o
+feedTextChars (TextInternal.Text arr off len) =
+  loop off
+  where
+    loop !off (Moore terminate progress) =
+      if off >= len
+        then
+          Moore terminate progress
+        else
+          TextArrayUtil.iter arr off $ \ char newOff ->
+            loop newOff (progress char)
