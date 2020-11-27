723a168c08761201fc56cdc43fbf0c41be1b92ec
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-11-27 13:13:14 +0300

    Add charText

--- library/MooreMachines.hs
+++ library/MooreMachines.hs
@@ -32,3 +32,38 @@ feedTextChars (TextInternal.Text arr off len) =
         else
           TextArrayUtil.iter arr off $ \ char newOff ->
             loop newOff (progress char)
+
+charText :: Moore Char Text
+charText =
+  next [] 0
+  where
+    next bytes arraySize =
+      Moore (terminate bytes arraySize) (progress bytes arraySize)
+    progress !bytes !arraySize char =
+      let
+        codepoint =
+          ord char
+        in if codepoint < 0x10000
+          then
+            next (fromIntegral codepoint : bytes) (succ arraySize)
+          else let
+            cpBasis =
+              codepoint - 0x10000
+            byte1 =
+              fromIntegral ((cpBasis `shiftR` 10) + 0xd800)
+            byte2 =
+              fromIntegral ((cpBasis .&. 0x3ff) + 0xdc00)
+            in next (byte2 : byte1 : bytes) (arraySize + 2)
+    terminate revListOfBytes arraySize =
+      TextPrivate.runText $ \freeze -> do
+        array <- TextArray.new arraySize
+        let
+          loop !offset bytes =
+            case bytes of
+              byte : bytesTail ->
+                TextArray.unsafeWrite array offset byte *>
+                loop (pred offset) bytesTail
+              [] ->
+                return ()
+          in loop (pred arraySize) revListOfBytes
+        freeze array arraySize
