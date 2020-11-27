0d1050f47d03bbc1567d50bdc6efb9ef280eade8
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-11-27 13:13:18 +0300

    Add tests

--- test/Main.hs
+++ test/Main.hs
@@ -10,10 +10,20 @@ import qualified Test.QuickCheck as QuickCheck
 import qualified Test.QuickCheck.Property as QuickCheck
 import qualified Data.Text as Text
 import qualified Data.Attoparsec.Text as Atto
-import qualified MooreMachines
+import qualified MooreMachines as Mm
 
 
 main =
   defaultMain $ testGroup "All" $ [
-    
+    testProperty "feedFoldable" $ \(input :: [Int]) ->
+      input ===
+      extract (Mm.feedFoldable input ask)
+    ,
+    testProperty "charText" $ \(input :: [Char]) ->
+      Text.pack input ===
+      extract (Mm.feedFoldable input Mm.charText)
+    ,
+    testProperty "feedTextChars" $ \(input :: Text) ->
+      input ===
+      extract (Mm.feedTextChars input Mm.charText)
     ]
